40ede712beb1675e30ec2e082a698d1161a7ea11
Stephan Bönnemann <stephan@boennemann.me>
2014-11-20 19:59:31 +0100

    feat: basic express setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+'use strict'
+
+var app = require('express')()
+var lib = require('./lib')
+
+lib.routes(app)
+
+var port = process.env.PORT || 5000
+app.listen(port, function() {
+  console.log('Listening on', port)
+})
